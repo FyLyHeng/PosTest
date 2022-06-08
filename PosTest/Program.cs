64a2cf4295afacb63b5c3811d913fd40ce19220e
fylyheng.phally <fylyheng.phally@gmail.com>
2022-06-08 14:44:33 +0700

    init

--- PosTest/Program.cs
+++ PosTest/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using IGPOS.Data.Models.UserRoleModel;
+using IGPOS.Data.Service.Auth;
+using IGPOS.Data.Service.Setting;
 Console.WriteLine("Hello, World!");
+var auth = new AuthenticationService();
+var user = new User {Username = "admin", Password = "123456"};
+auth.Login(user);
+
+
+Console.WriteLine($"cur:: {auth.GetCurrentUser().WorkShift?.Name}");
+var menuService = new MenuService();
+
+
+foreach (var menu in menuService.GetMenuByCurrentUser())
+{
+    Console.WriteLine($":: {menu.Color}");
+}
